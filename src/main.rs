a11a43e58dd2bc3b857b609812ac64d8b7793317
Kyllingene <fuzzymuffin343@gmail.com>
2023-01-15 15:36:44 -0800

    Fixed some bugs, it works fully now

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::{
     fs::{read_to_string, OpenOptions},
     io::Write,
+    path::Path,
     process::exit,
-    str::FromStr, path::Path,
+    str::FromStr,
 };
 
+use dirs::home_dir;
 use sarge::*;
 use todo_lib::*;
 
@@ -28,13 +30,6 @@ fn main() {
         Ok(r) => r,
     };
 
-    let filename: String;
-    if Path::new("todo.txt").exists() {
-        filename = String::from("todo.txt");
-    } else {
-        filename = String::from("~/todo.txt");
-    }
-
     if get_flag!(parser, both, 'h', "help") {
         println!("{} [options]", parser.binary.unwrap_or("todo".to_string()));
         println!("  -h /     --help        : prints this help message");
@@ -42,14 +37,36 @@ fn main() {
         println!("  -c / --complete <todo> : completes the todo, specified by the given text");
         println!("  -l /     --list        : prints this help message");
         println!("  -f /     --file <file> : specifies the file");
-        println!("                           if todo.txt is in the current directory, uses that;");
-        println!("                           otherwise, uses ~/todo.txt");
+        println!("                           if todo.txt exists in the current directory,");
+        println!("                           defaults to that; otherwise, uses ~/todo.txt");
 
         exit(0);
     }
 
+    let filename: String;
+    if let Some(ArgValue::String(f)) = get_val!(parser, both, 'f', "file") {
+        filename = f;
+    } else if Path::new("todo.txt").exists() {
+        filename = String::from("todo.txt");
+    } else {
+        let mut path = home_dir().unwrap_or_else(|| {
+            eprintln!("error: failed to get home directory");
+            exit(1);
+        });
+        path.push("todo.txt");
+
+        filename = path.display().to_string();
+    }
+
     let mut todos = TodoTable::new(None::<char>);
     todos.add_col("Todos");
+
+    let mut changed = false;
+    if get_val!(parser, both, 'n', "new").is_some()
+        || get_val!(parser, both, 'c', "complete").is_some()
+    {
+        changed = true;
+    }
 
     let todo_txt = match read_to_string(filename.clone()) {
         Ok(s) => s,
@@ -60,19 +77,6 @@ fn main() {
                 eprintln!("error (while reading file): {}", e);
                 exit(1);
             }
-        }
-    };
-
-    let mut file = match OpenOptions::new()
-        .create(true)
-        .write(true)
-        .truncate(true)
-        .open(filename)
-    {
-        Ok(file) => file,
-        Err(e) => {
-            eprintln!("error (while opening file to write): {}", e);
-            exit(1);
         }
     };
 
@@ -87,7 +91,6 @@ fn main() {
     }
 
     let mut action = false;
-    let mut changed = false;
     if let Some(ArgValue::String(todo)) = get_val!(parser, both, 'n', "new") {
         todos.add_todo(
             Todo::from_str(&todo).unwrap_or_else(|e| {
@@ -98,7 +101,6 @@ fn main() {
         );
 
         action = true;
-        changed = true;
     }
 
     if let Some(ArgValue::String(todo)) = get_val!(parser, both, 'c', "complete") {
@@ -111,7 +113,6 @@ fn main() {
             .complete();
 
         action = true;
-        changed = true;
     }
 
     if get_flag!(parser, both, 'l', "list") || !action {
@@ -121,6 +122,19 @@ fn main() {
     }
 
     if changed {
+        let mut file = match OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(filename)
+        {
+            Ok(file) => file,
+            Err(e) => {
+                eprintln!("error (while opening file to write): {}", e);
+                exit(1);
+            }
+        };
+
         todos.col("Todos").unwrap().todos.iter().for_each(|t| {
             writeln!(file, "{}", t.to_string()).unwrap_or_else(|e| {
                 eprintln!("error (while writing to file): {}", e);
